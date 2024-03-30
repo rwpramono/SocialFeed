09abc639f7d9af808efe166af6463fa386196c9c
Rachmat Wahyu Pramono <rwpramono@gmail.com>
2024-03-30 16:52:19 +0700

    Init QuickMockerAPITests

--- SocialFeedTests/Services/QuickMockerAPITests.swift
+++ SocialFeedTests/Services/QuickMockerAPITests.swift
@@ -5,4 +5,18 @@
 //  Created by Rachmat Wahyu Pramono on 30/03/24.
 //
 
-import Foundation
+import XCTest
+
+@testable import SocialFeed
+
+class QuickMockerAPITests: XCTestCase {
+    struct Dummy: Codable, Hashable {
+        let dummyValue: String
+    }
+    
+    func test_should_return_not_nil() throws {
+        let sut = QuickMockerAPI<Dummy>(.patch, path: "/dummy", body: ["dummy":"dummy"], query: ["dummy":"dummy"])
+        XCTAssertNotNil(sut.urlRequest)
+        XCTAssertNotNil(sut.urlComponent)
+    }
+}
