c1415c01c2991deb94846ef3e80d09927335e0ac
Rachmat Wahyu Pramono <rwpramono@gmail.com>
2024-03-30 07:14:00 +0700

    Prepare postlist screen

---
+++ SocialFeed/Features/PostsList/PostsListVC.swift
@@ -0,0 +1,19 @@
+//
+//  PostListVC.swift
+//  SocialFeed
+//
+//  Created by Rachmat Wahyu Pramono on 30/03/24.
+//
+
+import Foundation
+import UIKit
+
+class PostListVC: UIViewController {
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ SocialFeed/Features/PostsList/PostsListVM.swift
@@ -0,0 +1,8 @@
+//
+//  PostsListVM.swift
+//  SocialFeed
+//
+//  Created by Rachmat Wahyu Pramono on 30/03/24.
+//
+
+import Foundation
